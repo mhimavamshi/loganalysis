d9c8692d0448eb7fbd857ae211160cd37126594f
mhimavamshi <himavamshi1212@gmail.com>
2024-12-06 20:49:57 +0530

    test script

--- tester.py
+++ tester.py
@@ -20,11 +20,3 @@ print("Output.json:")
 print(b)
 print()
 print("Matches:", a == b)
-
-from loganalyzer import Parser
-
-p = Parser()
-
-print(p.lightweight_parse('192.168.1.100 - - [03/Dec/2024:10:12:46 +0000] "POST /login HTTP/1.1" 401 128 "Invalid credentials"'))
-print(p.lightweight_parse('198.51.100.23 - - [03/Dec/2024:10:12:49 +0000] "POST /feedback HTTP/1.1" 200 128'))
-
