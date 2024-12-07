e42f9d9eea0a391f0ef07392674711ea3b040c3d
mhimavamshi <himavamshi1212@gmail.com>
2024-12-07 13:58:09 +0530

    added docstrings

--- loganalyzer.py
+++ loganalyzer.py
@@ -16,10 +16,17 @@ class Parser:
         self.pattern = re.compile(r'(?P<ip>\d+\.\d+\.\d+\.\d+) - - \[(?P<date>.*?)\] "(?P<method>\S+) (?P<endpoint>\S+) HTTP/\S+" (?P<status>\d{3}) (?P<bytes>\d+)(?: "(?P<message>.*?)")?')
 
     def parse(self, line: str) -> dict:
+        '''
+        Uses a precompiled regex and generates a dictionary of the ip address, date, method, endpoint, status, bytes, and message
+        '''
         match = re.match(self.pattern, line)
         return match.groupdict()
     
     def lightweight_parse(self, line: str) -> tuple:
+        '''
+        IP address, endpoint and message are parsed from a line using split, join and array indexing
+        '''
+
         parts = line.split()
         ip = parts[0]
         endpoint = parts[6]
@@ -37,12 +44,25 @@ class Analyzer:
 
     # @line_profiler.profile
     def _figure_out_lines(self, lines, num_of_cores, size, scale_factor=4, min_lines=5_00_000, max_lines=1_00_00_000): 
+        '''
+        Depending on number of cores, previously sent lines and queue size, a new number of lines to be "chunked" is returned.
+
+        If the queue size is less than the number of cores - number of lines are decreased depending on scale factor so lines can be read faster and put in the queue
+        If the queue size is more than the number of cores - number of lines are increased depending on scale factor so reading them would be slower and queue size is maintained 
+        '''
+        
         if size < num_of_cores:
             return max(lines // scale_factor, min_lines) 
         return min(int(lines * scale_factor), max_lines)
 
     # @line_profiler.profile
     def _read_file_chunks(self, file_path, shared_queue: multiprocessing.Queue):
+        '''
+        File is read out in chunks i.e. a group of lines and put in the shared_queue so it can be processed.
+        
+        Number of lines is figured out during execution
+        '''
+
         num_of_cores = multiprocessing.cpu_count()
         initial_lines = 50_00_000
         with open(file_path, "r") as file:
@@ -58,6 +78,11 @@ class Analyzer:
 
     # @line_profiler.profile
     def _process_chunk(self, shared_queue, results_queue):
+        '''
+        A chunk of lines are taken from the shared queue, and "locally" processed using counters to get request count, endpoint accesses and failed logins
+        The counters are put into the results queue
+        '''
+
         local_request_count = Counter() 
         local_endpoint_accesses = Counter()   
         local_failed_logins = Counter()
@@ -83,6 +108,9 @@ class Analyzer:
         
     # @line_profiler.profile    
     def _collect_results(self, results_queue):
+        '''
+        From the results queue the "locally" processed counters are taken/collected and the global counters of request count, endpoint accesses and failed logins are updated
+        '''
         while not results_queue.empty():
             data = results_queue.get()
             self.request_count.update(data[0])
@@ -91,6 +119,9 @@ class Analyzer:
 
     # @line_profiler.profile
     def _pool_collect_results(self, result):
+        '''
+        Updates the global counters from a result of the map operation using a process pool
+        '''
         self.request_count.update(result[0])
         self.endpoint_accesses.update(result[1])
         self.failed_logins.update(result[2])
@@ -98,6 +129,9 @@ class Analyzer:
 
     # @line_profiler.profile
     def _pool_read_file_chunks(self, file_path, chunk_size):
+        '''
+        Reads a fixed chunk of lines from the file and returns it; Used as a generator
+        '''
         with open(file_path, "r") as file:
             while True:
                 lines = file.readlines(chunk_size)
@@ -108,6 +142,10 @@ class Analyzer:
 
     # @line_profiler.profile
     def _pool_process_chunk(self, chunk):
+        '''
+        Processes a chunk of line and returns a counter of request counts, endpoint accesses and failed logins
+        '''
+
         local_request_count = Counter()
         local_endpoint_accesses = Counter() 
         local_failed_logins = Counter()  
@@ -126,6 +164,11 @@ class Analyzer:
 
     # @line_profiler.profile
     def multiprocess_analyze_pool(self, file_path, return_data):
+        '''
+        Uses ProcessPoolExecutor to parallely map a chunk processing function to chunks of lines in file_path
+        Returns the data in a dictionary if needed
+        '''
+        
         print("INFO: Using pool")
         chunks = self._pool_read_file_chunks(file_path, chunk_size=5_00_000)  
 
@@ -144,6 +187,12 @@ class Analyzer:
 
     # @line_profiler.profile
     def multiprocess_analyze(self, file_path, return_data):
+        '''
+        Uses multiprocessing to parallely parse and retrieve the endpoint accesses, request counts, and failed login information from the file_path.
+        Returns the data as dictionary if needed
+        Uses Queues to share data among processes and collects the local data into global data
+        '''
+
         print("INFO: Using a shared queue")
 
         shared_queue = multiprocessing.Queue()
@@ -173,10 +222,19 @@ class Analyzer:
 
     
     def is_big_file(self, file_path):
+        '''
+        Returns true if the provided file is greater than 10 MB 
+        '''
         return os.path.getsize(file_path) > 10 * 1024 * 1024 
     
     # @line_profiler.profile
     def single_analyze_dynamic(self, file_path, return_data):
+        '''
+        Reads the file in chunks of lines and parses and retrieves the request counts, endpoint accesses, failed logins.
+        Returns the data if needed.
+        Adjusts the chunk size dynamically based on processing time 
+        '''
+
         import time
         try:
             print("INFO Using single core, and dynamic chunking")  
@@ -231,11 +289,14 @@ class Analyzer:
 
    
     # @line_profiler.profile
-    def single_analyze(self, file_path, return_data):
+    def single_analyze(self, file_path, return_data, chunk_size=7_00_000):
+        '''
+        Reads the file in chunks of lines and parses and retrieves the request counts, endpoint accesses, failed logins.
+        Returns the data if needed.
+        '''
         try:
             print("INFO: Using single core")  
             with open(file_path, "r") as file:
-                chunk_size = 7_00_000
                 while chunk := file.readlines(floor(chunk_size)):
 
                     for line in chunk: 
@@ -268,6 +329,10 @@ class Analyzer:
 
     # @line_profiler.profile
     def analyze_file(self, file_path, return_data=False, multi=True):
+        '''
+        Depending on the file size and number of cores available, single or multi processing is chosen and request counts, endpoint accesses and failed logins are retrieved from the file.
+        The retrieved data is stored in the object attributes - request_counts, endpoint_accesses and failed_logins 
+        '''
         pool = False
 
         if multiprocessing.cpu_count() > 1 and self.is_big_file(file_path) and multi:
@@ -278,6 +343,9 @@ class Analyzer:
         return self.single_analyze(file_path, return_data)    
 
     def print(self):
+        '''
+        Pretty prints the retrieved data to the console
+        '''
 
         print(f"INFO: Using threshold for suspicious logins: {self.threshold}")
         print()
@@ -317,6 +385,9 @@ class Analyzer:
     
     def _write_request_count(self, writer):
         # writer.writerow(["Requests per IP"])
+        '''
+        Writes the request counts using writer
+        '''
         writer.writerow([])
         writer.writerow(["IP Address", "Request Count"])
         for ip, count in self.request_count.items():
@@ -324,6 +395,9 @@ class Analyzer:
         writer.writerow([])
 
     def _write_endpoint_accesses(self, writer):
+        '''
+        Writes the endpoint accesses using writer
+        '''
         # writer.writerow(["Most Accessed Endpoint"])
         writer.writerow([])
         writer.writerow(["Endpoint", "Access Count"])
@@ -332,6 +406,9 @@ class Analyzer:
         writer.writerow([])
 
     def _write_failed_logins(self, writer):
+        '''
+        Writes the failed login attempts using writer
+        '''
         # writer.writerow(["Suspicious Activity"])
         writer.writerow([])
         writer.writerow(["IP Address", "Failed Login Count"])
@@ -340,6 +417,10 @@ class Analyzer:
                 writer.writerow([ip, count])       
 
     def write_to_csv(self, file_path):
+        '''
+        Writes the retrieved data to the CSV file at file_path
+        '''
+
         with open(file_path, mode="w", newline="\n") as csvfile:
             writer = csv.writer(csvfile)
 
@@ -380,12 +461,12 @@ def main():
     print()
     print(f"Data successfully written to {csv_file_path}")
 
-    testing = True
-    if testing:
-        import json
-        with open("output.json", "w") as file:
-            json.dump(raw_data, file)
-        print("INFO: output written to output.json")
+    # testing = False
+    # if testing:
+    #     import json
+    #     with open("output.json", "w") as file:
+    #         json.dump(raw_data, file)
+    #     print("INFO: output written to output.json")
     
 if __name__ == "__main__":
     main()
